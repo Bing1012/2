e04a4ae79602ac516df7a4d995b60fe1a76580ae
Bing1012 <99121491+Bing1012@users.noreply.github.com>
2022-03-10 20:11:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -379,6 +379,6 @@ if __name__ == "__main__":
         main(start_data, end_data)
     alt.themes.enable("default")
       
-backgroundColor="#00FF7F"
+st.backgroundColor="#00FF7F"
 
 # one_month, ten_days
